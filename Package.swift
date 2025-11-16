dc137417e86e4b27f8992ecf8f23ec6378e98928
spartan-minhnguyen <129926628+spartan-minhnguyen@users.noreply.github.com>
2025-11-16 14:39:58 +0700

    Update checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     .binaryTarget(
       name: "CompiletSDK",
       url: "https://github.com/compilet-dev/ios-compilet-sdk/releases/download/1.0.0/CompiletSDK.xcframework.zip",
-      checksum: "9479831b414bd4355af70912cf9b6f668eac9fe001d43fc30ced7f4f02820657"
+      checksum: "354d123d522763faceb1002d01b77d199699088945a997191d32b49a610a9423"
     )
   ]
 )
